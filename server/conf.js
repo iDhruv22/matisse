eb1ec8092714f21987195a00614ee26f68f2379b
Nikhilesh Kashibhatla Venkat <nikhilesh.kashibhatla@imaginea.com>
2016-08-16 15:27:49 +0530

    added comments for appId

--- server/conf.js
+++ server/conf.js
@@ -1,7 +1,7 @@
 module.exports = {
   fb: {
     appId: '1774620442754364',
-    appSecret: 'd3cc88ac79e449d889755c2fdeef1f61'
+    appSecret: 'd3cc88ac79e449d889755c2fdeef1f61' //generated appId and appSecret using Nikhilesh's facebook account
   },
   twit: {
     appId: 'R3PrOIhQ3ZI0NenirXVn3w',
@@ -9,6 +9,6 @@ module.exports = {
   },
   google: {
     appId: '1028734274402-0l841h4odfr1n262djqt5u2ag29vlqn3.apps.googleusercontent.com',
-    appSecret: 'pidmWKrenuzNtNZ2UkaigktS'
+    appSecret: 'pidmWKrenuzNtNZ2UkaigktS' //generated appId and appSecret using email ID:coolnikhilesh@gmail.com
   }
 };
